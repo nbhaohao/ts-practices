6aceb119d8256fddf4a0006338fc76dd1f0d37ce
nbhaohao <nbhaohao@163.com>
2022-01-13 09:56:34 +0800

    feat: 添加方法装饰器

--- src/decorator/index.ts
+++ src/decorator/index.ts
@@ -1,12 +1,27 @@
 function FirstClassDecorator(targetCLass: any) {
   console.log("targetClass.name:", targetCLass.name);
-  (new targetCLass()).buy();
+  new targetCLass().buy();
 }
 
-@FirstClassDecorator
+function TestMethod(
+  targetClass: any,
+  methodName: string,
+  dataProps: PropertyDescriptor
+) {
+  const tempMethod = dataProps.value;
+  dataProps.value = function (...params: any[]) {
+    console.log("插入自定义方法逻辑");
+    tempMethod.apply(this, params);
+  };
+}
+
+// @FirstClassDecorator
 class CustomService {
   name: string = "下单";
+  @TestMethod
   buy() {
     console.log(`${this.name}购买`);
   }
 }
+
+new CustomService().buy();
